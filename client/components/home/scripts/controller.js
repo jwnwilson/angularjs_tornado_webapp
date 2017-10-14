e94f319b5059ae026753681ac0ffcfdb14486f41
jwnwilson <jwnwilson@hotmail.co.uk>
2017-10-14 09:32:56 +0100

    Adding db reset script and removing unused divs from work

--- client/components/home/scripts/controller.js
+++ client/components/home/scripts/controller.js
@@ -1,33 +1,47 @@
 function HomeController($scope, $window, _mqWidth) {
   var self = this;
-  self.cv_text = "CV";
-  self.github_text = "Github";
-  self.linkedin_text = "Linked In";
   self.hide_text_limit = _mqWidth.tabletPortrait;
-  $scope.cv_text = self.cv_text;
-  $scope.github_text = self.github_text;
-  $scope.linkedin_text = self.linkedin_text;
   $scope.context = $window.CONTEXT;
 
+  self.cv_text = $scope.context.cv_text || "CV";
+  self.github_text = $scope.context.github_text || "Github";
+  self.linkedin_text = $scope.context.linkedin_text || "Linked In";
+  self.instagram_text =  $scope.context.instagram_text || "Instagram";
+
   if( $window.innerWidth < self.hide_text_limit ){
-    $scope.github_text = "";
-    $scope.linkedin_text = "";
+    set_links(true);
+  }
+  else{
+    set_links(false);
   }
 
   var appWindow = angular.element($window);
   appWindow.bind("resize", hide_text);
 
+  function set_links(clear) {
+    if( clear ){
+      $scope.cv_text = "";
+      $scope.github_text = "";
+      $scope.linkedin_text = "";
+      $scope.instagram_text = "";
+    }
+    else{
+      $scope.cv_text = self.cv_text;
+      $scope.github_text = self.github_text;
+      $scope.linkedin_text = self.linkedin_text;
+      $scope.instagram_text = self.instagram_text;
+    }
+  }
+
   function hide_text () {
     if( $window.innerWidth < self.hide_text_limit ){
       $scope.$apply(function() {
-        $scope.github_text = "";
-        $scope.linkedin_text = "";
+        set_links(true);
       });
     }
     else{
       $scope.$apply(function() {
-        $scope.github_text = self.github_text;
-        $scope.linkedin_text = self.linkedin_text;
+        set_links(false);
       });
     }
   }
---
+++ ops/reset_db.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+echo "Clearing nw_db"
+mongo nw_db --host db --eval "db.dropDatabase();"
+echo "Cleared nw_db"
+
+colls=( projects pages  )
+
+for c in ${colls[@]}
+do
+  mongoimport --host db -d nw_db --collection $c --type json --file /data/$c.json --jsonArray
+  sleep 2
+done
+
+echo "Reloaded data"
