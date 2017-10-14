bea505652133a3d18ae43563c94d9b818453a0e7
jwnwilson <jwnwilson@hotmail.co.uk>
2017-10-15 00:13:48 +0100

    Adding web projects data

--- client/components/work/scripts/controller.js
+++ client/components/work/scripts/controller.js
@@ -2,11 +2,19 @@
 
 function WorkController($scope, $window, WorkService) {
   $scope.context = $window.CONTEXT;
-  $scope.projects = [];
+  $scope.vfx_projects = [];
+  $scope.web_projects = [];
 
   WorkService.getProjects().then(function(data) {
     if( data.data ){
-      $scope.projects = data.data;
+      for ( var index in data.data ){
+        if( data.data[index].category == "vfx"){
+          $scope.vfx_projects.push(data.data[index]);
+        }
+        if( data.data[index].category == "web"){
+          $scope.web_projects.push(data.data[index]);
+        }
+      }
     }
   });
 }
