89dc573ed5b91e7f9afc47cc09dd98859cfe6016
jwnwilson <jwnwilson@hotmail.co.uk>
2017-10-26 10:07:34 +0100

    Fixed paging and tidied up blog code

--- client/components/blog/controllers/blogController.js
+++ client/components/blog/controllers/blogController.js
@@ -2,6 +2,9 @@
 
 function BlogController($http, $scope, $log){
   var blog = this;
+
+  // Variables
+
   blog.title = "Blog";
   blog.posts = [];
   blog.tab = "blog";
@@ -11,7 +14,19 @@ function BlogController($http, $scope, $log){
   $scope.currentPage = 1;
   $scope.pageSize = 3;
 
-  $scope.doCtrlPagingAct = function(text, page, pageSize, total) {
+  // Functions
+
+  blog.paging = paging;
+  blog.getBlogPosts = getBlogPosts;
+  blog.selectTab = selectTab;
+  blog.isSelected = isSelected;
+  blog.addPost = addPost;
+
+  // Initialisation
+
+  blog.getBlogPosts();
+
+  function paging(text, page, pageSize, total) {
     $log.info({
       text: text,
       page: page,
@@ -22,34 +37,35 @@ function BlogController($http, $scope, $log){
     var pageEnd = pageStart + pageSize;
     blog.filteredPosts = blog.posts.slice(pageStart, pageEnd);
     console.log(blog.filteredPosts);
-  };
+  }
 
-  $http.get("https://s3-us-west-2.amazonaws.com/s.cdpn.io/110131/posts_1.json")
-    .then(function(data){
-      console.log("Getting data");
-      blog.posts = data.data;
-      blog.filteredPosts = blog.posts.slice(0, $scope.pageSize);
-      $scope.total = data.data.length;
-    });
+  function getBlogPosts() {
+    $http.get("https://s3-us-west-2.amazonaws.com/s.cdpn.io/110131/posts_1.json")
+      .then(function(data){
+        console.log("Getting data");
+        blog.posts = data.data;
+        blog.filteredPosts = blog.posts.slice(0, $scope.pageSize);
+        $scope.total = data.data.length;
+      });
+  }
 
-  blog.selectTab = function(setTab){
+  function selectTab(setTab) {
     blog.tab = setTab;
     console.log(blog.tab);
-  };
+  }
 
-  blog.isSelected = function(checkTab){
+  function isSelected(checkTab) {
     return blog.tab === checkTab;
-  };
+  }
 
-  blog.addPost = function(){
+  function addPost() {
     blog.post.createdOn = Date.now();
     blog.post.comments = [];
     blog.post.likes = 0;
     blog.posts.unshift(this.post);
     blog.tab = 0;
     blog.post = {};
-  };
-
+  }
 }
 
 angular.module("Blog")
--- client/components/blog/controllers/commentController.js
+++ client/components/blog/controllers/commentController.js
@@ -5,7 +5,7 @@ function CommentController(){
   this.addComment = function(post){
     this.comment.createdOn = Date.now();
     post.comments.push(this.comment);
-    this.comment ={};
+    this.comment = {};
   };
 }
 
