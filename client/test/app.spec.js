12a459e11fea1bc6d72b68eba1b2767a6da2e71a
jwnwilson <jwnwilson@hotmail.co.uk>
2017-11-26 23:48:32 +0000

    Defining http requests

--- client/test/app.spec.js
+++ client/test/app.spec.js
@@ -1,49 +1,66 @@
 describe('App Tests', function() {
-  var controller, rootScope, location, route, $log;
+  var controller, rootScope, location, route, $log, httpBackend;
 
   beforeEach(function () {
       module('app');
 
-      inject(function (_$log_, $controller, $rootScope, $location, $route) {
+      inject(function (_$log_, $controller, $rootScope, $location, $route, $httpBackend) {
         $log = _$log_;
         controller = $controller;
         rootScope = $rootScope;
         location = $location;
         route = $route;
+        httpBackend = $httpBackend;
       });
     });
 
   describe('routing', function() {
     it('should route to home', function() {
+      var url = 'static/components/home/templates/home.html';
+      httpBackend.when('GET', url)
+        .respond(200, {});
+
       location.path('/');
       rootScope.$digest();
 
       expect(route.current.templateUrl)
-        .toBe('static/components/home/templates/home.html');
+        .toBe(url);
     });
 
     it('should route to blog', function() {
+      var url = 'static/components/blog/templates/blog.html';
+      httpBackend.when('GET', url)
+        .respond(200, {});
+
       location.path('/blog');
       rootScope.$digest();
 
       expect(route.current.templateUrl)
-        .toBe('static/components/blog/templates/blog.html');
+        .toBe(url);
     });
 
     it('should route to hobbies', function() {
+      var url = 'static/components/hobbies/templates/hobbies.html';
+      httpBackend.when('GET', url)
+        .respond(200, {});
+
       location.path('/hobbies');
       rootScope.$digest();
 
       expect(route.current.templateUrl)
-        .toBe('static/components/hobbies/templates/hobbies.html');
+        .toBe(url);
     });
 
     it('should route to work', function() {
+      var url = 'static/components/work/templates/work.html';
+      httpBackend.when('GET', url)
+        .respond(200, {});
+
       location.path('/work');
       rootScope.$digest();
 
       expect(route.current.templateUrl)
-        .toBe('static/components/work/templates/work.html');
+        .toBe(url);
     });
   });
 });
