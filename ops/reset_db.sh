61b334bbe1094a0661ac4db71401f4252a1e4060
jwnwilson <jwnwilson@hotmail.co.uk>
2017-11-26 16:03:38 +0000

    Changing data paths

--- ops/reset_db.sh
+++ ops/reset_db.sh
@@ -15,9 +15,9 @@ colls=( projects pages hobbies blog )
 for c in ${colls[@]}
 do
   if [[ -z "$DB_HOST" ]]; then
-    mongoimport -d nw_db --collection $c --type json --file /data/$c.json --jsonArray
+    mongoimport -d nw_db --collection $c --type json --file server/db/data/$c.json --jsonArray
   else
-    mongoimport --host $DB_HOST -d nw_db --collection $c --type json --file /data/$c.json --jsonArray
+    mongoimport --host $DB_HOST -d nw_db --collection $c --type json --file server/db/data/$c.json --jsonArray
   fi
   sleep 2
 done
